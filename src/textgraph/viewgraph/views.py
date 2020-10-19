4cc02de7996798de051d6285a5c94e7123fd94d5
Rou En Lee <jeanleerouen@gmail.com>
2020-10-20 07:52:13 +0800

    fix movie indexing

--- src/textgraph/viewgraph/views.py
+++ src/textgraph/viewgraph/views.py
@@ -18,7 +18,8 @@ def process_user_input(request):
             user_choices.append(int(query))
     
     user_data = compile_json_object(user_choices)
-    print(user_data)
+    
+
     dataJSON = json.dumps(user_data)
     
     return render(request,'viewgraph/graph.html', {'data': dataJSON})
@@ -35,8 +36,7 @@ def compile_json_object(chosen_movie_idx):
     nodes = data['nodes']
     links = data['links']
 
-    # for c in chosen_movie_idx:
-    #     chosen_nodes.append(nodes[c])
+    
         
     for L in links:
         if (L['source_idx'] in chosen_movie_idx) or (L['target_idx'] in chosen_movie_idx):
@@ -48,10 +48,11 @@ def compile_json_object(chosen_movie_idx):
             if L['target_idx'] not in movie_idx:
                 movie_idx.append(L['target_idx'])
 
+    
     for i in movie_idx:
         chosen_nodes.append(nodes[i])
 
-    
+
     userJSON = {"nodes" : chosen_nodes, "links" : chosen_links}
 
     print('========================================')
@@ -62,11 +63,11 @@ def compile_json_object(chosen_movie_idx):
 
 
 data = {"nodes": [
-    {"id": "127 Hours", "genre": "biography, drama", "genre_value": 0.06640975177288055, "scaled_genre_value": 180}, 
-    {"id": "A Little Help", "genre": "comedy", "genre_value": 0.030338691547513008, "scaled_genre_value": 51}, 
-    {"id": "Adventures of Power", "genre": "comedy", "genre_value": 0.030338691547513008, "scaled_genre_value": 51}, 
-    {"id": "The A-Team", "genre": "action, adventure", "genre_value": 0.015871849842369556, "scaled_genre_value": 0},
-    {"id": "Alice in Wonderland", "genre": "family, fantasy", "genre_value": 0.04498301912099123, "scaled_genre_value": 103}
+    {"idx" : 0, "id": "127 Hours", "genre": "biography, drama", "genre_value": 0.06640975177288055, "scaled_genre_value": 180}, 
+    {"idx" : 1, "id": "The A-Team", "genre": "action, adventure", "genre_value": 0.015871849842369556, "scaled_genre_value": 0},
+    {"idx" : 2, "id": "A Little Help", "genre": "comedy", "genre_value": 0.030338691547513008, "scaled_genre_value": 51}, 
+    {"idx" : 3, "id": "Adventures of Power", "genre": "comedy", "genre_value": 0.030338691547513008, "scaled_genre_value": 51}, 
+    {"idx" : 4, "id": "Alice in Wonderland", "genre": "family, fantasy", "genre_value": 0.04498301912099123, "scaled_genre_value": 103}
     ], 
 "links": [
     {"source_idx": 0, "target_idx": 2, "source": "127 Hours", "target": "A Little Help", "value": 0.41885014157709405}, 
@@ -76,4 +77,5 @@ data = {"nodes": [
     {"source_idx": 1, "target_idx": 4, "source": "The A-Team", "target": "Alice in Wonderland", "value": 0.4401263842713225}, 
     {"source_idx": 2, "target_idx": 3, "source": "A Little Help", "target": "Adventures of Power", "value": 0.5257436095080503}, 
     {"source_idx": 2, "target_idx": 4, "source": "A Little Help", "target": "Alice in Wonderland", "value": 0.43601514521144}, 
-    {"source_idx": 3, "target_idx": 4, "source": "Adventures of Power", "target": "Alice in Wonderland", "value": 0.4389207520427561}]}
+    {"source_idx": 3, "target_idx": 4, "source": "Adventures of Power", "target": "Alice in Wonderland", "value": 0.4389207520427561}
+    ]}
